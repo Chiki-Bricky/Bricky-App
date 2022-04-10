3b0d71a66490d6ebeae4bd95cb211f11f25893a0
Titrom25 <titrom25@mail.ru>
2022-04-10 13:48:33 +0700

    Add ServerAPI class

--- lib/Pages/take_picture_screen.dart
+++ lib/Pages/take_picture_screen.dart
@@ -1,16 +1,11 @@
 // @dart=2.9
 import 'dart:io';
-import 'dart:convert';
-import 'dart:typed_data';
-import 'package:http/http.dart' as http;
-
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import '../send_image.dart';
+import 'package:http/http.dart' as http;
 import 'package:path/path.dart' as path;
 import 'package:path_provider/path_provider.dart' as syspaths;
-
-import 'package:flutter/services.dart' show rootBundle;
+import '../send_image.dart';
 
 class TakePictureScreen extends StatefulWidget {
   static const routeName = '/take-picture';
@@ -23,35 +18,10 @@ class TakePictureScreen extends StatefulWidget {
 
 class _TakePictureScreenState extends State<TakePictureScreen> {
   File _pickedImage;
-  String text;
+  String serverResponse;
 
   void onSelectImage(File pickedImage) {
     _pickedImage = pickedImage;
-  }
-
-  Future<void> _sendPhoto() async {
-    Image img = Image.asset(_pickedImage.path);
-    Uint8List imageBytes =
-        (await rootBundle.load(_pickedImage.path)).buffer.asUint8List();
-    String base64Image = base64Encode(imageBytes);
-
-    final url = Uri.parse('http://192.168.31.234:5000/proccessImage');
-    http
-        .post(
-      url,
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8'
-      },
-      body: jsonEncode({
-        'image': base64Image,
-      }),
-    )
-        .then((response) {
-      print("Response status: ${response.statusCode}");
-      setState(() {
-        text = "Response status: ${response.statusCode}";
-      });
-    });
   }
 
   Future<void> _takePictureFromCamera() async {
@@ -76,6 +46,20 @@ class _TakePictureScreenState extends State<TakePictureScreen> {
 
     setState(() {
       _pickedImage = savedImage;
+    });
+  }
+
+  Future<void> _sendPhoto() async {
+    if (_pickedImage == null) {
+      setState(() {
+        serverResponse = "No image provided!";
+      });
+      return;
+    }
+    http.Response response = await ServerAPI.sendPhoto(_pickedImage);
+    print("Response status: ${response.statusCode}");
+    setState(() {
+      serverResponse = "Response status: ${response.statusCode}";
     });
   }
 
@@ -127,7 +111,7 @@ class _TakePictureScreenState extends State<TakePictureScreen> {
             label: const Text('Send Photo'),
             onPressed: _sendPhoto,
           ),
-          Text(text ?? 'No request'),
+          Text(serverResponse ?? 'No request'),
         ],
       ),
     );
--- lib/send_image.dart
+++ lib/send_image.dart
@@ -1,37 +1,28 @@
 // @dart=2.9
 import 'dart:convert';
 import 'dart:io';
-
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter/services.dart' show rootBundle;
 
-class Send with ChangeNotifier {
-  // File storedImage;
+class ServerAPI with ChangeNotifier {
+  static Future<http.Response> sendPhoto(File pickedImage) async {
+    Image img = Image.asset(pickedImage.path);
+    Uint8List imageBytes =
+        (await rootBundle.load(pickedImage.path)).buffer.asUint8List();
+    String base64Image = base64Encode(imageBytes);
 
-  // Send(this.storedImage);
-
-  Future<void> sendPhoto(File storedImage) async {
-    if (storedImage == null) {
-      print('Oops...');
-    } else {
-      List<int> imageBytes = await storedImage.readAsBytesSync();
-      String base64Image = base64Encode(imageBytes);
-
-      final url = Uri.parse('http://127.0.0.1:5000/proccessImage');
-      http
-          .post(
-        url,
-        body: json.encode({
-          'image': base64Image,
-        }),
-      )
-          .then(
-        (response) {
-          print(json.decode(response.body));
-          //add to list
-          // _items.insert(0, newProduct); // at the start of the list
-        },
-      );
-    }
+    final url = Uri.parse('http://192.168.31.234:5000/proccessImage');
+    final response = await http.post(
+      url,
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8'
+      },
+      body: jsonEncode({
+        'image': base64Image,
+      }),
+    );
+    return response;
   }
 }
