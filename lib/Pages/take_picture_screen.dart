e03ad5c529fd4f65fbda6d6c559bb6a35998a2dc
Titrom25 <titrom25@mail.ru>
2022-04-27 18:36:43 +0700

    Add border drawing

--- lib/Pages/take_picture_screen.dart
+++ lib/Pages/take_picture_screen.dart
@@ -1,4 +1,5 @@
 // @dart=2.9
+import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
@@ -6,6 +7,7 @@ import 'package:http/http.dart' as http;
 import 'package:path/path.dart' as path;
 import 'package:path_provider/path_provider.dart' as syspaths;
 import '../send_image.dart';
+import 'dart:math';
 
 class TakePictureScreen extends StatefulWidget {
   static const routeName = '/take-picture';
@@ -13,15 +15,59 @@ class TakePictureScreen extends StatefulWidget {
   TakePictureScreen();
 
   @override
-  _TakePictureScreenState createState() => _TakePictureScreenState();
+  TakePictureScreenState createState() => TakePictureScreenState();
 }
 
-class _TakePictureScreenState extends State<TakePictureScreen> {
-  File _pickedImage;
+class AddBorder extends StatelessWidget {
+  String className;
+  double LU_X = 0.0, LU_Y = 0, RD_X = 0, RD_Y = 0;
+  List colors = [
+    Colors.red,
+    Colors.green,
+    Colors.yellow,
+    Colors.blue,
+    Colors.pink,
+    Colors.orange,
+    Colors.lime
+  ];
+  Random random = new Random();
+
+  AddBorder(this.className, this.LU_X, this.LU_Y, this.RD_X, this.RD_Y) {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Stack(
+      textDirection: TextDirection.ltr,
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.only(left: LU_X, top: LU_Y),
+          padding: EdgeInsets.only(left: RD_X, top: RD_Y),
+          decoration: BoxDecoration(
+              border: Border.all(color: colors[random.nextInt(colors.length)])),
+          child: Text(className),
+        ),
+      ],
+    ));
+  }
+
+  void setState(int Function() param0) {}
+}
+
+class TakePictureScreenState extends State<TakePictureScreen> {
+  static File pickedImage;
   String serverResponse;
 
-  void onSelectImage(File pickedImage) {
-    _pickedImage = pickedImage;
+  List coordinateLU_X = [];
+  List coordinateLU_Y = [];
+  List coordinateRD_X = [];
+  List coordinateRD_Y = [];
+  List classNames = [];
+  List borders = [];
+  bool isVisibleCircle = false;
+
+  void onSelectImage(File selectedImage) {
+    pickedImage = selectedImage;
   }
 
   Future<void> _takePictureFromCamera() async {
@@ -45,21 +91,41 @@ class _TakePictureScreenState extends State<TakePictureScreen> {
         await File(imageFile.path).copy('${appDir.path}/$fileName');
 
     setState(() {
-      _pickedImage = savedImage;
+      pickedImage = savedImage;
     });
   }
 
   Future<void> _sendPhoto() async {
-    if (_pickedImage == null) {
+    coordinateLU_X.clear();
+    coordinateLU_Y.clear();
+    coordinateRD_X.clear();
+    coordinateRD_Y.clear();
+
+    if (pickedImage == null) {
       setState(() {
         serverResponse = "No image provided!";
       });
       return;
     }
-    http.Response response = await ServerAPI.sendPhoto(_pickedImage);
+    http.Response response = await ServerAPI.sendPhoto(pickedImage);
     print("Response status: ${response.statusCode}");
+
+    final Map parsed = json.decode(response.body);
+    final bricks = parsed['bricks'];
+
     setState(() {
-      serverResponse = "Response status: ${response.statusCode}";
+      serverResponse =
+          "Response status: ${response.statusCode}, Bricks founded: ${bricks.length}";
+      for (int i = 0; i < bricks.length; i++) {
+        final singleBrick = bricks[i];
+        print(singleBrick);
+        classNames.add(singleBrick['class']);
+        coordinateLU_X.add(singleBrick['xmin']);
+        coordinateLU_Y.add(singleBrick['ymin']);
+        coordinateRD_X.add(singleBrick['xmax'] - singleBrick['xmin']);
+        coordinateRD_Y.add(singleBrick['ymax'] - singleBrick['ymin']);
+      }
+      isVisibleCircle = true;
     });
   }
 
@@ -72,24 +138,37 @@ class _TakePictureScreenState extends State<TakePictureScreen> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Container(
-            height: 500,
-            decoration: BoxDecoration(
-              border: Border.all(width: 3, color: Colors.grey),
+        children: [
+          Stack(children: <Widget>[
+            Container(
+              height: 500,
+              decoration: BoxDecoration(
+                border: Border.all(width: 3, color: Colors.grey),
+              ),
+              child: pickedImage != null
+                  ? Image.asset(
+                      pickedImage.path,
+                      fit: BoxFit.cover,
+                      width: double.infinity,
+                    )
+                  : const Text(
+                      'No Image Taken',
+                      textAlign: TextAlign.center,
+                    ),
+              alignment: Alignment.center,
             ),
-            child: _pickedImage != null
-                ? Image.asset(
-                    _pickedImage.path,
-                    fit: BoxFit.cover,
-                    width: double.infinity,
-                  )
-                : const Text(
-                    'No Image Taken',
-                    textAlign: TextAlign.center,
-                  ),
-            alignment: Alignment.center,
-          ),
+            if (isVisibleCircle)
+              for (int i = 0; i < classNames.length; i++)
+                if (isVisibleCircle)
+                  AddBorder(
+                      classNames[i],
+                      coordinateLU_X[i].toDouble() *
+                          ((MediaQuery.of(context).size.width - 3 * 2) / 1024),
+                      coordinateLU_Y[i].toDouble() * ((500) / 1024),
+                      coordinateRD_X[i].toDouble() *
+                          ((MediaQuery.of(context).size.width - 3 * 2) / 1024),
+                      coordinateRD_Y[i].toDouble() * ((500) / 1024)),
+          ]),
           const SizedBox(
             height: 20,
           ),
--- lib/send_image.dart
+++ lib/send_image.dart
@@ -9,16 +9,16 @@ import 'package:image/image.dart' as IMG;
 
 class ServerAPI with ChangeNotifier {
   static Future<http.Response> sendPhoto(File pickedImage) async {
-    File imageCopy =
-        await pickedImage.copy(pickedImage.parent.path + '/tmp.jpg');
-    IMG.Image img = IMG.decodeImage(await imageCopy.readAsBytes());
-    IMG.Image thumbnail = IMG.copyResize(img, width: 256, height: 256);
-    await imageCopy.writeAsBytes(IMG.encodeJpg(thumbnail));
+    // File imageCopy =
+    // await pickedImage.copy(pickedImage.parent.path + '/tmp.jpg');
+    // IMG.Image img = IMG.decodeImage(await imageCopy.readAsBytes());
+    // IMG.Image thumbnail = IMG.copyResize(img, width: 256, height: 256);
+    // await imageCopy.writeAsBytes(IMG.encodeJpg(thumbnail));
     Uint8List imageBytes =
-        (await rootBundle.load(imageCopy.path)).buffer.asUint8List();
+        (await rootBundle.load(pickedImage.path)).buffer.asUint8List();
     String base64Image = base64Encode(imageBytes);
 
-    final url = Uri.parse('http://192.168.31.234:5000/proccessImage');
+    final url = Uri.parse('http://10.192.18.34:5000/proccessImage');
     final response = await http.post(
       url,
       headers: <String, String>{
